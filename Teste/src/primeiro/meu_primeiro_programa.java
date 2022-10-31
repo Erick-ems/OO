694be6490560d92de5335e6089b64e22a106efb8
Erick-ems <79948051+Erick-ems@users.noreply.github.com>
2022-10-31 13:50:29 -0300

    Commit/Manual 31/10/22
    Meu primeiro commit, foi feito de forma manual pois o mesmo não permitiu meu acesso

---
+++ Teste/src/primeiro/meu_primeiro_programa.java
@@ -0,0 +1,13 @@
+package primeiro;
+
+public class meu_primeiro_programa {
+
+	public static void main(String[] args) {
+		System.out.print("Meu Primeiro programa em JAVA");
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Meu Primeiro programa em JAVA");
+	}
+
+}
